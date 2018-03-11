86228cb34432c900e216f1193fed495306bdb04e
martypdx <marty@martypdx.com>
2018-03-11 16:59:37 -0700

    add route

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const client = require('./db-client');
+
+app.get('/todos', (request, response) => {
+    client.query(`
+        SELECT id, task, completed FROM todos;
+    `)
+        .then(result => response.send(result.rows))
+        .catch(console.error);
+});
 
 app.listen(PORT, () => {
     console.log('app server started on port', PORT);
